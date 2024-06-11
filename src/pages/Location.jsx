131c8f39c145a75441dedaa2269de97f8f116c10
Deepakshiva05 <2016017@saec.ac.in>
2024-06-11 17:22:57 +0530

    aos of location removed

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -12,7 +12,7 @@ const Location = () => {
         <h1 className='text-center text-white text-4xl font-rich' id="topic">Our Global <span className='text-orange-700'>Locations</span></h1>
 
             <div className="cards flex justify-evenly flex-wrap mb-5">
-                <div className="card w-[18rem] mt-5" data-aos="fade-up" data-aos-duration="1000">
+                <div className="card w-[18rem] mt-5">
                     
                     <div className="card-img-top">
                         <img src={nw} className='rounded-4xl h-[5cm] w-100'></img>
@@ -27,7 +27,7 @@ const Location = () => {
 
                 </div>
                 </div>
-                <div className="card w-[18rem] mt-5" data-aos="fade-up" data-aos-duration="1000">
+                <div className="card w-[18rem] mt-5">
                     
                     <div className="card-img-top">
                         <img src={london} className='rounded-4xl h-[5cm] w-100'></img>
@@ -42,7 +42,7 @@ const Location = () => {
 
                 </div>
                 </div>
-                <div className="card w-[18rem] mt-5" data-aos="fade-up" data-aos-duration="1000">
+                <div className="card w-[18rem] mt-5">
                     
                     <div className="card-img-top">
                         <img src={chennai} className='rounded-4xl h-[5cm] w-100'></img>
